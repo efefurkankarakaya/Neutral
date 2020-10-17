b980cd91d9084cdefe174e7e3f19819b3ad8d8ea
efefurkankarakaya <efkdev@outlook.com>
2020-10-17 17:58:13 +0300

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,55 @@
+#!/bin/bash
+"""
+[Desktop Entry]
+Name = Firefox Developer Edition
+GenericName = Firefox Developer Edition
+Terminal = false
+Icon = /opt/firefox-dev/browser/chrome/icons/default/default128.png
+Type = Application
+Categories = Application; Network; Web-Development;
+Comment = Web Browser for Developers
+Exec = /opt/firefox-dev/firefox
+StartupWMClass = Firefox Developer Edition
+"""
+
 sudo echo "Installation starts..."
 FILE=firefox-dev.tar.bz2
 FOLDER=firefox-dev
+EXEC_PATH=/opt/firefox-dev
+
+sudo ./uninstall.sh
+
+install(){
+    tar -xjf firefox-dev.tar.bz2
+    mv firefox firefox-dev
+    sudo mv firefox-dev /opt
+
+    echo "[Desktop Entry]
+    Name = Mozilla Firefox Developer Edition
+    GenericName = Mozilla Firefox Developer Edition
+    Exec = /opt/firefox-dev/firefox
+    Terminal = false
+    Icon = /opt/firefox-dev/browser/chrome/icons/default/default128.png
+    Type = Application
+    Categories = Application; Network; Web-Development;
+    Comment = Web Browser for Developers" > firefox-dev.desktop
+
+    sudo cp firefox-dev.desktop $HOME/Desktop
+    # sudo mv $HOME/Desktop/firefox-dev-desktop $HOME/Desktop/firefox-dev.desktop
+
+    sudo mv firefox-dev.desktop $HOME/.local/share/applications
+    # sudo mv $HOME/.local/share/applications/firefox-dev-desktop $HOME/.local/share/applications/firefox-dev.desktop
+}
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
+    # echo "$FILE found."
+    # echo "$FILE removing.."
+    # rm $FILE
+    # echo "Download is starting.."
+    wget -O firefox-dev.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"
 else
+    echo "Download is starting.."
     wget -O firefox-dev.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"
 fi
 
@@ -12,24 +57,6 @@ if [ -f "$FOLDER" ]; then
     rm "$FOLDER"
 fi
 
-tar -xjf firefox-dev.tar.bz2
-mv firefox firefox-dev
-sudo mv firefox-dev /opt
-
-echo "[Desktop Entry]
-Name = Mozilla Firefox Developer Edition
-GenericName = Mozilla Firefox Developer Edition
-Exec = /opt/firefox-dev/firefox
-Terminal = false
-Icon = /opt/firefox-dev/browser/chrome/icons/default/default128.png
-Type = Application
-Categories = Application; Network; Web-Development;
-Comment = Web Browser for Developers" > firefox-dev-desktop
-
-sudo cp firefox-dev-desktop $HOME/Desktop
-sudo mv $HOME/Desktop/firefox-dev-desktop $HOME/Desktop/firefox-dev.desktop
-
-sudo mv firefox-dev-desktop $HOME/.local/share/applications
-sudo mv $HOME/.local/share/applications/firefox-dev-desktop $HOME/.local/share/applications/firefox-dev.desktop
+install()
 
 echo "Now you can access Firefox Developer Edition from your Start Menu or Desktop."
--- uninstall.sh
+++ uninstall.sh
@@ -1,24 +1,36 @@
-sudo echo "Uninstalling Mozilla Firefox Developer Edition..."
+sudo echo "File scan is starting.."
 
 FILE=firefox-dev.tar.bz2
-DIRECTORY=/opt/firefox-dev/
+EXEC_PATH=/opt/firefox-dev/
 DESKTOP=$HOME/Desktop/firefox-dev.desktop
 ICON=$HOME/.local/share/applications/firefox-dev.desktop
 
 if [ -f "$FILE" ]; then
+    echo "$FILE found."
+    echo "$FILE removing.."
     rm "$FILE"
+    echo "$FILE removed."
 fi
 
-if [ -d "$DIRECTORY" ]; then
-    sudo rm -r "$DIRECTORY"
+if [ -d "$EXEC_PATH" ]; then
+    echo "$EXEC_PATH found."
+    echo "$EXEC_PATH removing.."
+    sudo rm -r "$EXEC_PATH"
+    echo "$EXEC_PATH removed."
 fi
 
 if [ -f "$DESKTOP" ]; then
+    echo "$DESKTOP found."
+    echo "$DESKTOP removing.."
     rm "$DESKTOP"
+    echo "$DESKTOP removed."
 fi
 
 if [ -f "$ICON" ]; then
+    echo "$ICON found."
+    echo "$ICON removing.."
     rm "$ICON"
+    echo "$ICON removed."
 fi
 
 echo "Uninstalled."
